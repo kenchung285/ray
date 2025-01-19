0764a0b62142a4b71ff4e8a07afb33e53b9e2049
Cheng-Yeh Chung <kenchung285@gmail.com>
2025-01-19 22:12:19 +0800

    [Refactor] using list(invocation_metadata())
    Signed-off-by: Cheng-Yeh Chung <kenchung285@gmail.com>

--- python/ray/serve/grpc_util.py
+++ python/ray/serve/grpc_util.py
@@ -18,9 +18,7 @@ class RayServegRPCContext:
         self._auth_context = grpc_context.auth_context()
         self._code = grpc_context.code()
         self._details = grpc_context.details()
-        self._invocation_metadata = [  # noqa: C416
-            (key, value) for key, value in grpc_context.invocation_metadata()
-        ]
+        self._invocation_metadata = list(grpc_context.invocation_metadata())
         self._peer = grpc_context.peer()
         self._peer_identities = grpc_context.peer_identities()
         self._peer_identity_key = grpc_context.peer_identity_key()
